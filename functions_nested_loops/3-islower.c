5cd78fda240d731941a54f06b5fe54a4a861efac
GastonVargas21 <gastonvargas1238@gmail.com>
2025-07-06 21:09:04 +0000

    task 0

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,3 +1,9 @@
+/**
+ * _islower - Verifica si un carácter es minúscula
+ * @c: el carácter a verificar
+ *
+ * Return: 1 si c es una letra minúscula, 0 en caso contrario
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
